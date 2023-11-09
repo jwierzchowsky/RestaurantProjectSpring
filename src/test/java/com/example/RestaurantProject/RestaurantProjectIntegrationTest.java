1cb974d62717e1fd1ea3da5c9c3658a089af4dd1
Jan Wierzchowski <s95606@pollub.edu.pl>
2023-11-09 16:09:49 +0100

    feature/Integration tests 2

--- src/test/java/com/example/RestaurantProject/RestaurantProjectIntegrationTest.java
+++ src/test/java/com/example/RestaurantProject/RestaurantProjectIntegrationTest.java
@@ -6,18 +6,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import org.springframework.http.MediaType;
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = RestaurantProjectApplicationTests.class)
+@AutoConfigureMockMvc
+@EnableAutoConfiguration(exclude= SecurityAutoConfiguration.class)
 
 @AutoConfigureTestDatabase
 public class RestaurantProjectIntegrationTest {
@@ -41,5 +53,10 @@ public class RestaurantProjectIntegrationTest {
         List<OrderEntity> found = repository.findAll();
         assertThat(found).extracting(OrderEntity::getName).containsOnly("Schabowy");
 
+    }
+
+    private void createTestOrder(String name, int quantity, Date orderDate){
+        OrderEntity order = new OrderEntity(name, quantity, orderDate);
+        repository.saveAndFlush(order);
     }
 }
